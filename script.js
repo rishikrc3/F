85b2c310fa562a26f82ac37dd986ad4e97c5e3fb
rishik <rishikrc3@gmail.com>
2025-10-16 19:39:07 +0100

    fix: update form structure and improve AJAX submission handling

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-$(document).ready(function () {
-    console.log("Script loaded!");
-  $("#studentForm").on("submit", function (e) {
-    e.preventDefault();
+$(document).ready(function()
+{
+    console.log("hhelo");
+    $("#studentForm").submit(function(e){
+        e.preventDefault();
 
+        console.log("Hi");
+        const studentData = {
+          firstName: $("#firstName").val(),
+          lastName:$("#lastName").val(),
+          email:$("#email").val()
+        };
 
-    const studentData = {
-      FirstName: $("#firstName").val(),
-      LastName: $("#lastName").val(),
-      Email: $("#email").val(),
-    };
-    console.log(studentData)
-
-
-    $.ajax({
-      url: "http://localhost:5019/api/Student/addNewStudent",
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(studentData),
-      success: function (response) {
-        alert("Student added successfully!");
-        $("#studentForm")[0].reset();
-      },
-      error: function (xhr, status, error) {
-        alert("Error: " + error);
-        console.log(xhr.responseText);
-      },
-    });
-  });
-});
-
+        $("#buttons").addClass("d-none");
+         $("#spinner").removeClass("d-none");
+        
+        $.ajax({
+          url: "http://localhost:5019/api/Test/postData",
+          type: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(studentData),
+          success: function () {
+            $("#buttons").removeClass("d-none");
+            $("#spinner").addClass("d-none");
+            $("#successToast").removeClass("d-none");
+            setTimeout(function () {
+              $("#successToast").addClass("d-none");
+            }, 3000);
+          },
+          error: function () {
+            $("#buttons").removeClass("d-none");
+            $("#spinner").addClass("d-none");
+            $("#errorToast").removeClass("d-none");
+            setTimeout(function () {
+              $("#errorToast").addClass("d-none");
+            }, 3000);
+          },
+        });
+        
+    }
+);
+    
+})
